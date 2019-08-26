871e27d92dda659b8d5ed7210680070b787c9a92
vcfam <boysthatbrunch@gmail.com>
2019-08-26 15:33:26 -0400

    cleaned up

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/home/rviglian/Projects/anniversary/sr10/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/home/rviglian/Projects/anniversary/sr10/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
