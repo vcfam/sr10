1d9c489275d5fc6f549a40876641ab3fcf3c9182
vcfam <boysthatbrunch@gmail.com>
2020-03-11 13:18:51 -0400

    Updates

---
+++ precache-manifest.85670feda71b5ff09785e1d81c2a3c67.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30443e48fd0de29f67529ee00ad2c9dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aff43476e7ec224d4f70",
+    "url": "/static/css/main.5a99d102.chunk.css"
+  },
+  {
+    "revision": "03629b576b008140ef20",
+    "url": "/static/js/2.edf578f3.chunk.js"
+  },
+  {
+    "revision": "aff43476e7ec224d4f70",
+    "url": "/static/js/main.d76d0a5f.chunk.js"
+  },
+  {
+    "revision": "14b652a939ddcc47c991",
+    "url": "/static/js/runtime~main.f7492b2e.js"
+  },
+  {
+    "revision": "bc073bd629570ce564a95d083217e7c5",
+    "url": "/static/media/CollageWed.bc073bd6.png"
+  },
+  {
+    "revision": "0a66332cfc6e7ad84ad079fa4fb29be7",
+    "url": "/static/media/bg.0a66332c.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "38cfcf27e244f11977cb69402bebb0cb",
+    "url": "/static/media/logo.38cfcf27.svg"
+  },
+  {
+    "revision": "ae71cd6455e461bd27101d94ce42613a",
+    "url": "/static/media/map1.ae71cd64.png"
+  },
+  {
+    "revision": "358447795e7a7a79d24330f8d3447880",
+    "url": "/static/media/map2.35844779.png"
+  },
+  {
+    "revision": "edfbd92a9373446816d95ab6bba5b6ef",
+    "url": "/static/media/map3.edfbd92a.png"
+  },
+  {
+    "revision": "8237636726ace980a00020ea5eed6f8d",
+    "url": "/static/media/orsini.82376367.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e910107c8038f87a41c7d8e7387ccae1.js"
+  "/precache-manifest.85670feda71b5ff09785e1d81c2a3c67.js"
 );
 
 self.addEventListener('message', (event) => {
